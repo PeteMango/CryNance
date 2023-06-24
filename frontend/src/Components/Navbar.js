65eac18ae5862233c1068480b6d3fc6a58f309e0
zhoujacob <113641223+zhoujacob@users.noreply.github.com>
2023-06-24 19:08:04 -0400

    Updated the NavBar and little Quality of Lifes

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,27 +1,38 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import React from 'react';
 
-const client_secret = process.env.client_secret
-const AUTH_URL = "https://id.worldcoin.org/authorize?client_id=app_staging_0af5473e8e0e5c8eae581173d8a04603&client_secret=${client_secret}&response_type=code&redirect_uri=http://localhost:3000/#/"
+const client_secret = process.env.client_secret;
+const AUTH_URL = `https://id.worldcoin.org/authorize?client_id=app_staging_0af5473e8e0e5c8eae581173d8a04603&client_secret=${client_secret}&response_type=code&redirect_uri=http://localhost:3000/#/`;
 
 const Navbar = () => {
-    return (
-        <>
-            <div className="navbar bg-base-100">
-                <div className="navbar-start">
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-                </div>
-                
-                <div className="navbar-center">
-                  <Link to="/create" className="btn btn-primary">Create</Link>
-                </div>
-                
-                <div className="navbar-end">
-                    <a href={AUTH_URL} className="btn">Login</a>
-                </div>
-            </div>      
-        </>
-    );
+  const history = useHistory();
+
+  const handleLogoClick = () => {
+    history.push('/');
+  };
+
+  return (
+    <nav className="navbar bg-base-100 flex justify-between items-center px-4">
+      <div className="navbar-start">
+        <button className="btn btn-ghost normal-case text-xl" onClick={handleLogoClick}>
+          CRyNance
+        </button>
+      </div>
+      <div className="navbar-center flex-auto space-x-2">
+        <Link to="/browse" className="btn btn-primary transition duration-300 ease-in-out transform hover:scale-105">
+          Browse
+        </Link>
+        <Link to="/create" className="btn btn-primary transition duration-300 ease-in-out transform hover:scale-105">
+          Create
+        </Link>
+      </div>
+      <div className="navbar-end">
+        <a href={AUTH_URL} className="btn btn-secondary transition duration-300 ease-in-out transform hover:scale-105">
+          Login
+        </a>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
