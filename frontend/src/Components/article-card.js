6b499cdf1d9a9d054040b9fc1f53ce91386fb1f8
Sophia Sun <53122458+sophiiasun@users.noreply.github.com>
2023-06-24 17:46:36 -0400

    Modal

--- frontend/src/Components/article-card.js
+++ frontend/src/Components/article-card.js
@@ -1,6 +1,6 @@
 import './Components.css'
 
-const ArticleCard = ({ header, body, imgLink, date, github, devpost }) => {
+const ArticleCard = ({ title, date, body, level }) => {
 	return (
 		<>
 			<div className="card">
@@ -11,8 +11,18 @@ const ArticleCard = ({ header, body, imgLink, date, github, devpost }) => {
 					<h4 className="cardBody">Chatify was a project developed for a Hackathon (Hack This Fall 2.0). Chatify users discuss global issues through socially tough times (e.g. Covid-19 pandemic). Join numerous rooms with different topics, or start their own room with a new topic. Built with React, Bootstrap, and Firebase.</h4>
 					<button className="cardButton"><a target="_blank">Upvote</a></button>
 					<button className="cardButton"><a target="_blank">Downvote</a></button>
+                    <button className="cardButton" onClick={()=>window.my_modal_2.showModal()}>Read</button>
 				</div>
 			</div>
+            <dialog id="my_modal_2" className="modal">
+                <form method="dialog" className="modal-box">
+                    <h3 className="font-bold text-lg">Pay with Worldcoin</h3>
+                    <p className="py-4">Press ESC key or click outside to close</p>
+                </form>
+                <form method="dialog" className="modal-backdrop">
+                    <button>close</button>
+                </form>
+            </dialog> 
 		</>
 	)
 }
--- frontend/src/Pages/Browse.js
+++ frontend/src/Pages/Browse.js
@@ -33,7 +33,6 @@ export default function Browse() {
                     </div>
                 </div>
                 <ArticleCard></ArticleCard>
-                <DashboardCard></DashboardCard>
             </div>
             {/* <Card></Card> */}
         </>
