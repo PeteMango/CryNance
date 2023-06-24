6886ebb5a6b3aa081f1f4974c3f3eda0002ae05d
Norman Chen <113641259+normanchenn@users.noreply.github.com>
2023-06-24 15:56:48 -0400

    so tilted

--- frontend/src/Pages/Create.js
+++ frontend/src/Pages/Create.js
@@ -1,30 +1,51 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
+// app.post("/api/add-article", async (req, res) => {
+//     // id author_id created_at content title level vote
+//     const {author_id, content, title, level, isDraft, categories} = req.body;
 
 export default function Create() {
-    const [isPopupOpen, setPopupOpen] = useState(false);
+    const [isPopupOpen, setPopupOpen] = useState(true);
     const [articleContent, setArticleContent] = useState('');
+    const [title, setTitle] = useState('');
+    const level = useRef('');
+    const user = localStorage.getItem('user');
+    const [query, setQuery] = useState("");
+    const [categories, setCategories] = useState([]);
 
-    const handleNewArticleClick = () => {
-        setPopupOpen(true);
-    };
+    // const handleNewArticleClick = () => {
+    //     setPopupOpen(true);
+    // };
 
-    const handleCancelClick = () => {
-        setPopupOpen(false);
-        setArticleContent('');
-    };
+    // const handleCancelClick = () => {
+    //     setPopupOpen(false);
+    //     setArticleContent('');
+    // };
 
-    const handleSaveAsDraftClick = () => {
-        // Logic to save article as draft
-        setPopupOpen(false);
-        setArticleContent('');
-    };
+    // const handleSaveAsDraftClick = () => {
+    //     // Logic to save article as draft
+    //     setPopupOpen(false);
+    //     setArticleContent('');
+    // };
 
-    const handlePublishClick = () => {
-        // Logic to publish article
-        setPopupOpen(false);
-        setArticleContent('');
-    };
+    // const handlePublishClick = () => {
+    //     // Logic to publish article
+    //     setPopupOpen(false);
+    //     setArticleContent('');
+    // };
+
+    const addCategory = (e) => {
+        e.preventDefault();
+        const newCategory = query;
+        setCategories([...categories, newCategory]);
+    }
+
+    useEffect(() => {
+        console.log(categories);
+    }, [categories])
+    useEffect(() => {
+        console.log(query);
+    }, [query])
 
     return (
         <div>
@@ -42,19 +63,29 @@ export default function Create() {
                 </div>
             </div>
 
-            <button onClick={handleNewArticleClick}>New Article</button>
+            {/* <button onClick={handleNewArticleClick}>New Article</button> */}
 
             {isPopupOpen && (
-                <div>
+                <form>
                     <h2>New Article</h2>
+                    {/* <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
                     <textarea
                         value={articleContent}
                         onChange={(e) => setArticleContent(e.target.value)}
+                        
                     ></textarea>
-                    <button onClick={handleCancelClick}>Cancel</button>
-                    <button onClick={handleSaveAsDraftClick}>Save as Draft</button>
-                    <button onClick={handlePublishClick}>Publish</button>
-                </div>
+                    <select ref={level}  >
+                        <option value="">Select Tier of Article</option>
+                        <option value="free">Free</option>
+                        <option value="premium">Premium</option>
+                        <option value="All Knower">All Knower</option>
+                    </select> */}
+                    <input type="text" value={query} onChange={(e) => setQuery(e.target.value)}/>
+                    <button onSubmit={addCategory}>Add Category</button>
+                    {/* <button onClick={handleCancelClick}>Cancel</button>
+                    <button type="submit" onClick={handleSaveAsDraftClick}>Save as Draft</button>
+                    <button type="submit" onClick={handlePublishClick}>Publish</button> */}
+                </form>
             )}
         </div>
     );
