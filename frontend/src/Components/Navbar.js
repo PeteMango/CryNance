a95dd9fcf78d9e248fd6e47148afe56b91ffd13d
Peter Wang <40186116+PeteMango@users.noreply.github.com>
2023-06-25 00:53:23 -0400

    frontend.

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,8 +1,10 @@
 import { Link, useHistory } from "react-router-dom";
+import { IDKitWidget } from "@worldcoin/idkit";
 import React from "react";
 
 const client_secret = process.env.client_secret;
 const AUTH_URL = `https://id.worldcoin.org/authorize?client_id=app_staging_0af5473e8e0e5c8eae581173d8a04603&client_secret=${client_secret}&response_type=code&redirect_uri=http://localhost:3000/#/`;
+const code = new URLSearchParams(window.location.search).get("code");
 
 const Navbar = () => {
   const history = useHistory();
@@ -33,6 +35,16 @@ const Navbar = () => {
             <Link to="/create" className="menu-item">
               Create
             </Link>
+          </li>
+          <li>
+            <IDKitWidget
+              app_id="app_staging_0af5473e8e0e5c8eae581173d8a04603" // obtain this from developer.worldcoin.org
+              action="my_action"
+              enableTelemetry
+              onSuccess={(result) => console.log(result)} // pass the proof to the API or your smart contract
+            >
+              {({ open }) => <button onClick={open}>Sign In</button>}
+            </IDKitWidget>
           </li>
         </ul>
       </div>
