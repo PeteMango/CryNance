901320f581a1934438e4ac56bc39a00e3cd393ed
Peter Wang <40186116+PeteMango@users.noreply.github.com>
2023-06-24 23:10:48 -0400

    THE IDKIT WORKS HUGE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
-import './App.css';
+import "./App.css";
 
-import { HashRouter as Router, Switch, Route } from 'react-router-dom'; 
-import { useEffect, useState } from 'react';
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { IDKitWidget } from "@worldcoin/idkit";
 
-import Navbar from './Components/Navbar'; 
-import Login from './Pages/Login';
-import Browse from './Pages/Browse';
-import Create from './Pages/Create';
+import Navbar from "./Components/Navbar";
+import Login from "./Pages/Login";
+import Browse from "./Pages/Browse";
+import Create from "./Pages/Create";
 
-const code = new URLSearchParams(window.location.search).get('code')
-const client_id = "app_staging_0af5473e8e0e5c8eae581173d8a04603"
-const client_secret = process.env.client_secret
+const code = new URLSearchParams(window.location.search).get("code");
+const client_id = "app_staging_0af5473e8e0e5c8eae581173d8a04603";
+const client_secret = process.env.client_secret;
 
 // import { PolybaseProvider } from "@polybase/react";
 // import { Polybase } from "@polybase/client";
@@ -18,36 +19,45 @@ const client_secret = process.env.client_secret
 // const polybase = new Polybase();
 
 function App() {
-  const [accessToken, setAccesstoken] = useState()
+  const [accessToken, setAccesstoken] = useState();
 
-  const data = new URLSearchParams()
-  data.append('code', {code})
-  data.append('grant_type', 'authorization_code')
+  const data = new URLSearchParams();
+  data.append("code", { code });
+  data.append("grant_type", "authorization_code");
   useEffect(() => {
-    fetch('https://id.worldcoin.org/token', {
-      mode: 'no-cors',
-      method: 'POST',
+    fetch("https://id.worldcoin.org/token", {
+      mode: "no-cors",
+      method: "POST",
       body: data,
-    }).then(res => {
-      console.log(data);
-      console.log(code);
-      console.log(res);
-      console.log(res.id_token);
-      setAccesstoken(res.data.id_token);
     })
-    .catch((err) => {
-      console.log(err);
-    }) 
-  }, [code])
+      .then((res) => {
+        console.log(data);
+        console.log(code);
+        console.log(res);
+        console.log(res.id_token);
+        setAccesstoken(res.data.id_token);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [code]);
   return (
     <div className="App">
       <Router>
+        <IDKitWidget
+          app_id="app_staging_0af5473e8e0e5c8eae581173d8a04603" // obtain this from developer.worldcoin.org
+          action="my_action"
+          enableTelemetry
+          onSuccess={(result) => console.log(result)} // pass the proof to the API or your smart contract
+        >
+          {({ open }) => <button onClick={open}>Click me</button>}
+        </IDKitWidget>
         <Navbar />
         <Switch>
-          <Route path = '/' exact component = {Login}/>
-          <Route path = '/browse' component = {Browse}/>
-          <Route path = '/create' component = {Create}/>
-        </Switch> 
+          <Route path="/" exact component={Login} />
+          <Route path="/browse" component={Browse} />
+          <Route path="/create" component={Create} />
+        </Switch>
       </Router>
       {/* {code ? (<DashBoard code = {code}></DashBoard>) : (<div></div>)} */}
     </div>
--- nextjs/app/page.js
+++ nextjs/app/page.js
@@ -1,11 +1,24 @@
+"use client"
 import Image from 'next/image'
 import Navbar from './components/Navbar'
 import DashboardCard from './components/DashboardCard'
 import ArticleCard from './components/ArticleCard'
-
+import { useCallback } from "react";
+import { IDKitWidget } from "@worldcoin/idkit";
 import { signIn, signOut, useSession } from "next-auth"
 
 export default function Home() {
+  const handleProof = useCallback((result) => {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(), 3000);
+        // NOTE: Example of how to decline the verification request and show an error message to the user
+    });
+  }, []);
+
+  const onSuccess = (result) => {
+      console.log(result);
+  };
+
   return (
     <div>
       <Navbar />
@@ -14,6 +27,24 @@ export default function Home() {
       {/* Add the rest of your home page content */}
       <h1>Welcome to the Home Page!</h1>
       {/* Additional content */}
+
+      <div>
+        <div className="container">
+              <div style={{ display: "flex", alignItems: "center", justifyContent: "center", minHeight: "100vh" }}>
+                  <IDKitWidget
+                      action="my_action"
+                      signal="my_signal"
+                      onSuccess={onSuccess}
+                      handleVerify={handleProof}
+                      app_id="app_f0125cbd47bd047ec18c80603b7479a2"
+                      walletConnectProjectId="e17f404fcad13e2596e3971475c1dc37"
+                  >
+                      {({ open }) => <button onClick={open}>Click me</button>}
+                  </IDKitWidget>
+              </div>
+          </div>
+      </div>
+
     </div>
   )
 }
