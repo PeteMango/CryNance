0233ec6cabec54d980a63b0045c4c3d3699c2807
Peter Wang <40186116+PeteMango@users.noreply.github.com>
2023-06-25 02:29:19 -0400

    stuff

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -2,9 +2,6 @@ import { Link, useHistory } from "react-router-dom";
 import { IDKitWidget } from "@worldcoin/idkit";
 import React from "react";
 
-import { useEffect, useState } from "react";
-import { IDKitWidget } from "@worldcoin/idkit";
-
 const client_secret = process.env.client_secret;
 const AUTH_URL = `https://id.worldcoin.org/authorize?client_id=app_staging_0af5473e8e0e5c8eae581173d8a04603&client_secret=${client_secret}&response_type=code&redirect_uri=http://localhost:3000/#/`;
 const code = new URLSearchParams(window.location.search).get("code");
@@ -22,20 +19,20 @@ const Navbar = () => {
       method: "POST",
       body: JSON.stringify({
         nullifier_hash: hash,
+      }),
+    })
+      .then((res) => {
+        console.log(res);
+        return res.json();
       })
-    })
-    .then((res) => {
-      console.log(res);
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data);
-      localStorage.setItem("username", data.username);
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-  }
+      .then((data) => {
+        console.log(data);
+        localStorage.setItem("username", data.username);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <nav className="navbar bg-base-100 flex justify-between items-center px-4">
       <div className="navbar-start">
@@ -49,8 +46,17 @@ const Navbar = () => {
       <div className="navbar-center flex-auto space-x-2"></div>
       <div className="navbar-end">
         <ul className="menu menu-horizontal px-1">
-          <li><Link to="/browse" className="menu-item">Browse</Link></li>
-          <li><Link to="/create" className="menu-item">Create</Link></li>
+          <li>
+            <Link to="/browse" className="menu-item">
+              Browse
+            </Link>
+          </li>
+          <li>
+            <Link to="/create" className="menu-item">
+              Create
+            </Link>
+          </li>
+          <li>
             <IDKitWidget
               app_id="app_staging_0af5473e8e0e5c8eae581173d8a04603" // obtain this from developer.worldcoin.org
               action="my_action"
