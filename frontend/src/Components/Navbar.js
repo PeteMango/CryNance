d2715809908ed2b0127207ac5da1820bd2516b90
zhoujacob <113641223+zhoujacob@users.noreply.github.com>
2023-06-25 02:31:46 -0400

    fixed css

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -2,9 +2,6 @@ import { Link, useHistory } from "react-router-dom";
 import { IDKitWidget } from "@worldcoin/idkit";
 import React from "react";
 
-import { useEffect, useState } from "react";
-import { IDKitWidget } from "@worldcoin/idkit";
-
 const client_secret = process.env.client_secret;
 const AUTH_URL = `https://id.worldcoin.org/authorize?client_id=app_staging_0af5473e8e0e5c8eae581173d8a04603&client_secret=${client_secret}&response_type=code&redirect_uri=http://localhost:3000/#/`;
 const code = new URLSearchParams(window.location.search).get("code");
@@ -51,6 +48,7 @@ const Navbar = () => {
         <ul className="menu menu-horizontal px-1">
           <li><Link to="/browse" className="menu-item">Browse</Link></li>
           <li><Link to="/create" className="menu-item">Create</Link></li>
+          <li>
             <IDKitWidget
               app_id="app_staging_0af5473e8e0e5c8eae581173d8a04603" // obtain this from developer.worldcoin.org
               action="my_action"
