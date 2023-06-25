09b24ada6b8e7a9aa7df42f388043545710984ae
Thomas <thomaswang003@gmail.com>
2023-06-25 04:40:08 -0400

    Make navbar dependent on signin

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,6 +1,7 @@
 import { Link, useHistory } from "react-router-dom";
 import { IDKitWidget } from "@worldcoin/idkit";
 import React from "react";
+import {useState, useEffect} from "react";
 
 const client_secret = process.env.client_secret;
 const AUTH_URL = `https://id.worldcoin.org/authorize?client_id=app_staging_0af5473e8e0e5c8eae581173d8a04603&client_secret=${client_secret}&response_type=code&redirect_uri=http://localhost:3000/#/`;
@@ -9,6 +10,8 @@ const code = new URLSearchParams(window.location.search).get("code");
 const Navbar = () => {
   const history = useHistory();
   let nullifier_hash, credential_type;
+  const [showResults, setShowResults] = useState(false); 
+
   const handleLogoClick = () => {
     history.push("/");
   };
@@ -34,7 +37,7 @@ const Navbar = () => {
       });
   };
   return (
-    <nav className="navbar bg-base-100 flex justify-between items-center px-4">
+      <nav className="navbar bg-base-100 flex justify-between items-center px-4">
       <div className="navbar-start">
         <button
           className="btn btn-ghost normal-case text-xl"
@@ -46,27 +49,31 @@ const Navbar = () => {
       <div className="navbar-center flex-auto space-x-2"></div>
       <div className="navbar-end">
         <ul className="menu menu-horizontal px-1">
-          <li><Link to="/browse" className="menu-item">Browse</Link></li>
-          <li><Link to="/create" className="menu-item">Create</Link></li>
-          <li>
-            <IDKitWidget
-              app_id="app_staging_0af5473e8e0e5c8eae581173d8a04603" // obtain this from developer.worldcoin.org
-              action="my_action"
-              enableTelemetry
-              onSuccess={(result) => {
-                console.log(result);
-                nullifier_hash = result.nullifier_hash;
-                credential_type = result.credential_type;
-                console.log(nullifier_hash);
-                console.log(credential_type);
-                localStorage.setItem("user", nullifier_hash);
-                localStorage.setItem("authentication", credential_type);
-                addUser();
-              }}
-            >
-              {({ open }) => <button onClick={open}>Sign In</button>}
-            </IDKitWidget>
-          </li>
+          {showResults ? <li><Link to="/browse" className="menu-item">Browse</Link></li> : null}
+          {showResults ? <li><Link to="/create" className="menu-item">Create</Link></li> : null}
+          {!showResults ? 
+            <li>
+              <IDKitWidget
+                app_id="app_staging_0af5473e8e0e5c8eae581173d8a04603" // obtain this from developer.worldcoin.org
+                action="my_action"
+                enableTelemetry
+                onSuccess={(result) => {
+                  console.log(result);
+                  nullifier_hash = result.nullifier_hash;
+                  credential_type = result.credential_type;
+                  console.log(nullifier_hash);
+                  console.log(credential_type);
+                  localStorage.setItem("user", nullifier_hash);
+                  localStorage.setItem("authentication", credential_type);
+                  addUser();
+                  setShowResults(true);
+                }}
+              >
+                {({ open }) => <button onClick={open}>Sign In</button>}
+              </IDKitWidget>
+            </li>
+            : null
+            }
         </ul>
       </div>
     </nav>
